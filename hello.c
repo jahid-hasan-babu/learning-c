c3aa977ae4b14bd9cbd6ce0078851bad68dad3d9
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-10-19 21:57:12 +0600

    first-code-done

---
+++ hello.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+//This is single lin comment
+/*
+This is multi line comment
+To show it in code
+*/
+/*int main(){
+    char star = '*';
+    int age = 22;
+    float pi = 3.14;
+    printf("Hello world \n");
+    printf("My age is %d \n", age);
+    printf("My pi is %f \n", pi);
+    printf("My star is %c \n", star);
+    return 0;
+}
+*/
+
+
+
+#include <stdio.h>
+// Tow int input and print sum
+/*int main() {
+    int a, b;
+    printf("Enter a: ");
+    scanf("%d", &a);
+
+    printf("Enter b: ");
+    scanf("%d", &b);
+
+    // int sum = a + b;
+    printf("Sum is = %d\n", a + b);
+    return 0;
+} */
+
+//area of square
+// int main(){
+//     int side;
+//     printf("Enter side of square: ");
+//     scanf("%d", &side);
+//     printf("Area of square is: %d", side*side);
+//     return 0;
+// }
+
+// Area of circle
+// int main(){
+//     float radius;
+//     printf("Enter radius of circle: ");
+//     scanf("%f", &radius);
+//     printf("Area of circle is: %f", 3.14*radius*radius);
+//     return 0;
+
+// }
+
+// Calculate perimeter of rectangle
+// int main(){
+//     int length, width;
+//     printf("Enter length: ");
+//      scanf("%d", &length);
+//     printf("Enter width: ");
+//     scanf("%d", width);
+//     printf("Perimeter of rectangle is %d", 2*(length + width));
+//     return 0;
+
+// }
+
+// cube number
+int main(){
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    printf("Cube of %d is %d\n", n, n * n * n);
+    return 0;
+}
