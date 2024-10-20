60c7631e666d087e40dc950326899a6164f0bcd5
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-10-20 10:39:49 +0600

    chapter 2

---
+++ chapter2.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+// int main(){
+//     int oldAge = 22;
+//     int years = 5;
+//     int a, b,c;
+//     a = b = c = 4;
+//     int newAge = oldAge + years;
+//     printf("%d \n", newAge);
+//     return 0;
+// }
+
+// int main(){
+//     int a = 4 * 3 / 6 * 2; // ans 4
+//     printf("%d \n", a);
+//     return 0;
+// }
+
+// int main(){
+//     int a = 5 * 2 - 2 *2; //ans 6
+//     printf("%d \n", a);
+//     return 0;
+// }
+
+// int main(){
+//     int a = 5 * 2 / 2 * 3; //ans 15
+//     printf("%d \n", a);
+//     return 0;
+// }
+
+// int main(){
+//     int a = 5 * (2 / 2 ) * 3; //ans 15
+//     printf("%d \n", a);
+//     return 0;
+// }
+
+// int main(){
+//     int a = 5 + 2 / 2  * 3; //ans 8
+//     printf("%d \n", a);
+//     return 0;
+// }
+
+// int main(){
+//     printf("%d \n", 4 == 4); //output 1
+//     return 0;
+// }
+
+int main(){
+    printf("%d \n", 4 != 4); //output 0
+    printf("%d \n", 4 != 3); //output 1
+    return 0;
+}
