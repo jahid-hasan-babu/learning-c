80ad4bf3606adca320236e0b647300f050253caa
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-10-26 11:30:32 +0600

    loop-done

---
+++ chapter4.c
@@ -0,0 +1,112 @@
+//loop
+#include<stdio.h>
+// int main(){ 
+//     for (int i = 10;  i >= 1; i--)
+//     {
+//        printf("%d \n",i);
+//     }
+//     return 0;
+    
+// }
+
+// int main(){ 
+//     for (int i = 0;  i <= 10; i++)
+//     {
+//        printf("%d \n",i);
+//     }
+//     return 0;
+    
+// }
+
+// int main(){ 
+//     for (char ch = 'a';  ch <= 'z'; ch++)
+//     {
+//        printf("%c \n",ch);
+//     }
+//     return 0;
+    
+// }
+
+// int main(){
+//     int n;
+//     printf("Enter a value: ");
+//     scanf("%d", &n);
+//     // int i = 0;
+//     // while( i <= n){ 
+//     //     printf("%d \n",i);      
+//     //     i++;
+//     // }
+
+//     for (int i = 0;  i <= n; i++)
+//     {
+//        printf("%d \n",i);
+//     }
+//     return 0;
+// }
+
+//do while
+// int main(){
+
+//     int i = 0;
+//     do
+//     {
+//        printf("%d \n",i);      
+//        i++;
+//     } while (i <= 5);
+//     return 0;
+// }
+
+// int main(){
+//     int n;
+//     printf("Enter a value: ");
+//     scanf("%d", &n);
+//     int sum = 0;
+//     for(int i = 0; i <= n; i++){
+//         sum += i; // sum = sum + i
+//     }
+//     printf("sum is = %d \n", sum);
+//     for(int i = n; i >= 1; i--){
+//         printf("%d \n",i);
+//     }
+//     return 0;
+// }
+
+
+// int main(){
+//     int n;
+//     printf("Enter a value: ");
+//     scanf("%d", &n);
+//     int sum = 0;
+//     for(int i = 0, j = n; i <= n && j >= 1; i++, j--){
+//         sum += i; 
+//         printf("%d \n",j);
+//     }
+//     printf("sum is = %d \n", sum);
+//     return 0;
+// }
+
+// int main(){
+//     int n;
+//     printf("Enter a value: ");
+//     scanf("%d", &n);
+//     int sum = 0;
+//     for(int j = n;  j >= 1;  j--){
+//         sum += j; 
+//         printf("%d \n",j);
+//     }
+//     printf("sum is = %d \n", sum);
+//     return 0;
+// }
+
+int main(){
+    int n;
+    printf("Enter a value: ");
+    scanf("%d", &n);
+    int sum = 0;
+    for(int i = 1;  i <= 10;  i++){
+       
+        printf("%d \n",n * i);
+    }
+ 
+    return 0;
+}
